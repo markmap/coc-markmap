08651c93901ae801f58b61ec99f9ab42a87d4598
Gerald <gera2ld@163.com>
2020-01-16 10:58:17 +0800

    chore: migrate to eslint

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,21 @@ import {
 } from 'coc.nvim';
 import { createMarkmap } from 'markmap-cli';
 
-export function activate(context: ExtensionContext) {
+async function getContent(nvim: Neovim, line1 = '1', line2 = '"$"'): Promise<string> {
+  const lines = (await nvim.eval(`getline(${line1},${line2})`)) as string[];
+  return lines.join('\n');
+}
+
+async function createMarkmapFromVim(nvim: Neovim, line1?: string, line2?: string): Promise<void> {
+  const content = await getContent(nvim, line1, line2);
+  const basename = await nvim.eval('expand("%<")');
+  createMarkmap({
+    content,
+    output: basename && `${basename}.html`,
+  });
+}
+
+export function activate(context: ExtensionContext): void {
   const { nvim } = workspace;
 
   context.subscriptions.push(workspace.registerKeymap(
@@ -37,18 +51,4 @@ export function activate(context: ExtensionContext) {
       await createMarkmapFromVim(nvim, line1, line2);
     },
   ));
-}
-
-async function createMarkmapFromVim(nvim: Neovim, line1?: string, line2?: string) {
-  const content = await getContent(nvim, line1, line2);
-  const basename = await nvim.eval('expand("%<")');
-  createMarkmap({
-    content,
-    output: basename && `${basename}.html`,
-  });
-}
-
-async function getContent(nvim: Neovim, line1: string = '1', line2: string = '"$"') {
-  const lines = (await nvim.eval(`getline(${line1},${line2})`)) as string[];
-  return lines.join('\n');
 }
